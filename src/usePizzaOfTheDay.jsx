75dc3df97f6a64574eb4aef20523491af78fe46d
aramay <kasper_341@hotmail.com>
2025-02-16 08:33:25 -0800

    add file for custom hook - usePizzaOfTheDay

---
+++ src/usePizzaOfTheDay.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export default usePizzaOfTheDay = () => {
+    const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null)
+
+    
+    async function () {
+        
+    }
+    
+    useEffect(() => {
+        async
+    })
+}
