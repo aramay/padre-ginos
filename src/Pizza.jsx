2c3e595b375d8f94869848945aecf35c7ae5af1d
aramay <kasper_341@hotmail.com>
2025-02-15 23:02:39 -0800

    Update component to read API data

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -2,18 +2,25 @@ const Pizza = ({ menu }) => {
   //   console.log(props);
   //   const { menu } = props;
   console.log(menu);
+  return (
+    <div className="pizza" key={menu.key}>
+      <h1>{menu.name}</h1>
+      <p>{menu.description}</p>
+      <img src={menu.image} alt={menu.name} />
+    </div>
+  );
 
-  const pizzaList = menu.map((menu, index) => {
-    return (
-      <div className="pizza" key={index}>
-        <h1>{menu.name}</h1>
-        <p>{menu.description}</p>
-        <img src={menu.img}></img>
-      </div>
-    );
-  });
+  // const pizzaList = menu.map((menu, index) => {
+  //   return (
+  //     <div className="pizza" key={index}>
+  //       <h1>{menu.name}</h1>
+  //       <p>{menu.description}</p>
+  //       <img src={menu.img}></img>
+  //     </div>
+  //   );
+  // });
 
-  return <>{pizzaList}</>;
+  // return <>{pizzaList}</>;
 };
 
 export default Pizza;
