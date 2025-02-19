66bbbbd8ac0784b03a48956ab931753da41ccf58
aramay <kasper_341@hotmail.com>
2025-02-19 10:11:38 -0800

    render component PizzaOfTheDay

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 // import React from "react";
 import { createRoot } from "react-dom/client";
+// import StrickMode from "react"
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
     <div>
       <Order />
+      <PizzaOfTheDay />
     </div>
   );
 };
