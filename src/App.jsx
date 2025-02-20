bdbeca443d4eae32a939a59293a793779e33054a
aramay <kasper_341@hotmail.com>
2025-02-20 11:18:04 -0800

    update className for logo

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PizzaOfTheDay from "./PizzaOfTheDay";
 const App = () => {
   return (
     <div>
+      <h1 className="logo">Padre Gino's Pizza</h1>
       <Order />
       <PizzaOfTheDay />
     </div>
